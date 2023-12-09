c16ba472a2e634f96bfe1afaa053aa10ee285938
Назар Захарія <nazariy04z@gmail.com>
2023-12-09 21:00:26 +0200

    Fix some problem

--- src/components/ModalWindow/Modal.jsx
+++ src/components/ModalWindow/Modal.jsx
@@ -6,7 +6,7 @@ function Modal({active, setActive, children}) {
   return (
       <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
           <div className={active ? 'modal-content active' : 'modal-content'} onClick={e => e.stopPropagation()}>
-              <img alt='icon-close' src={closeIcon} className='icon-close' onClick={() => setActive(false)}/>
+              <img alt='icon-close' src={closeIcon} className='close-icon' onClick={() => setActive(false)}/>
               <div className='children-wrapper'>
                 { children }
               </div>
